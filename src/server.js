45785189dc2f819a6ace63e89681bf48f8aa9366
Luan V <crackersage@gmail.com>
2021-04-09 00:45:29 -0400

    Second Commit

--- src/server.js
+++ src/server.js
@@ -8,7 +8,10 @@ server.set('view engine', 'ejs')
 //Habilitar arquivos statics
 server.use(express.static('public'))
 
-//ROtas
+//USar o req
+server.use(express.urlencoded({ extended: true }))
+
+//Rotas
 server.use(routes)
 
 server.listen(3000, 
